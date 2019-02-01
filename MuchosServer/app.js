74b68543b8b7fdee17f955d6699d632b8d9b6e63
srinskit <srinagrao2007@gmail.com>
2019-02-01 11:27:43 +0530

    Patched game.start array iteration, double-quoted single quotes

--- MuchosServer/app.js
+++ MuchosServer/app.js
@@ -1,23 +1,23 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const indexRouter = require('./routes/index');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -28,29 +28,29 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
 });
 
-const RachServer = require('./modules/RachServer/RachServer');
-const uuid_v1 = require('uuid/v1');
+const RachServer = require("./modules/RachServer/RachServer");
+const uuid_v1 = require("uuid/v1");
 
 let lobby = {};
 const services = {
-    '/version':
+    "/version":
         function (rach, on_err, on_result) {
-            on_result('0.0 dev');
+            on_result("0.0 dev");
         },
-    '/lobby.create':
+    "/lobby.create":
         function (lobby, rach, on_err, on_result, lobby_name) {
             try {
                 if (String(lobby_name).length === 0)
-                    on_err('invalid lobby name');
+                    on_err("invalid lobby name");
             } catch (e) {
-                on_err(e.message || 'invalid lobby name');
+                on_err(e.message || "invalid lobby name");
             }
             let id = uuid_v1();
             let lob = {
@@ -58,7 +58,7 @@ const services = {
                     id: id,
                     name: lobby_name,
                 },
-                player: {},
+                players: {},
                 game: {
                     started: false,
                     hand: {},
@@ -69,86 +69,88 @@ const services = {
             lobby[id] = lob;
             on_result(lob.core);
         }.bind(null, lobby),
-    '/lobby.join':
+    "/lobby.join":
         function (lobby, rach, on_err, on_result, lobby_id, user) {
             try {
                 if (String(lobby_id).length === 0 || lobby[lobby_id] == null)
-                    return on_err('invalid lobby id');
+                    return on_err("invalid lobby id");
             } catch (e) {
-                return on_err(e.message || 'invalid lobby id');
+                return on_err(e.message || "invalid lobby id");
             }
-            lobby[lobby_id].player[user.name] = user;
-            rach.pub(`/lobby/${lobby_id}/player_event`, {event: 'joined', user: user});
+            lobby[lobby_id].players[user.name] = user;
+            rach.pub(`/lobby/${lobby_id}/player_event`, {event: "joined", user: user});
             on_result(lobby[lobby_id].core);
         }.bind(null, lobby),
-    '/lobby.leave':
+    "/lobby.leave":
         function (lobby, rach, on_err, on_result, lobby_id, user) {
             try {
                 if (String(lobby_id).length === 0 || lobby[lobby_id] == null)
-                    return on_err('invalid lobby id');
+                    return on_err("invalid lobby id");
             } catch (e) {
-                return on_err(e.message || 'invalid lobby id');
+                return on_err(e.message || "invalid lobby id");
             }
-            delete lobby[lobby_id].player[user.name];
-            rach.pub(`/lobby/${lobby_id}/player_event`, {event: 'left', user: user});
-            on_result('done');
+            delete lobby[lobby_id].players[user.name];
+            rach.pub(`/lobby/${lobby_id}/player_event`, {event: "left", user: user});
+            on_result("done");
         }.bind(null, lobby),
-    '/lobby.mates':
+    "/lobby.players":
         function (lobby, rach, on_err, on_result, lobby_id) {
             try {
                 if (String(lobby_id).length === 0 || lobby[lobby_id] == null)
-                    return on_err('invalid lobby id');
+                    return on_err("invalid lobby id");
             } catch (e) {
-                return on_err(e.message || 'invalid lobby id');
+                return on_err(e.message || "invalid lobby id");
             }
-            on_result(lobby[lobby_id].player);
+            on_result(lobby[lobby_id].players);
         }.bind(null, lobby),
-    '/game.start':
+    "/game.start":
         function (lobby, rach, on_err, on_result, lobby_id) {
             try {
                 if (String(lobby_id).length === 0 || lobby[lobby_id] == null)
-                    return on_err('invalid lobby id');
+                    return on_err("invalid lobby id");
             } catch (e) {
-                return on_err(e.message || 'invalid lobby id');
+                return on_err(e.message || "invalid lobby id");
             }
-            let players = Object.keys(lobby[lobby_id].player), hand = {};
+            if (lobby[lobby_id].game.started)
+                return on_err("game in progress");
+            let players = Object.keys(lobby[lobby_id].players), hand = {};
             if (players.length < 2) {
-                return on_err('not enough players');
+                return on_err("not enough players");
             }
             let cards = get_all_cards();
             shuffle(cards);
-            for (let player in players)
+            for (let player of players)
                 hand[player] = cards.splice(0, 7);
             lobby[lobby_id].game.deck = cards;
             lobby[lobby_id].game.hand = hand;
-            let turn_order = Object.keys(lobby[lobby_id].player);
+            let turn_order = Object.keys(lobby[lobby_id].players);
             shuffle(turn_order);
             lobby[lobby_id].game.turn_order = turn_order;
-            for (let player in players)
-                rach.pub(`/game/${lobby_id}/p/${player}`,
+            for (let player of players)
+                rach.pub(`/game/${lobby_id}/private/${player}`,
                     {
-                        type: 'initHand',
+                        event: "initHand",
                         hand: hand[player]
                     });
-            rach.pub(`/game/${lobby_id}/event`, {type: 'turn', player: turn_order[0]});
+            rach.pub(`/game/${lobby_id}/broadcast`, {event: "turn", player: turn_order[0]});
             lobby[lobby_id].game.started = true;
-            on_result('done');
+            on_result("done");
         }.bind(null, lobby),
-    '/game.move':
+    "/game.move":
         function (lobby, rach, on_err, on_result, lobby_id, user, move) {
             try {
                 if (String(lobby_id).length === 0 || lobby[lobby_id] == null)
-                    return on_err('invalid lobby id');
+                    return on_err("invalid lobby id");
             } catch (e) {
-                return on_err(e.message || 'invalid lobby id');
+                return on_err(e.message || "invalid lobby id");
             }
             let game = lobby[lobby_id].game;
             if (game.started !== true)
-                return on_err('game not started yet');
+                return on_err("game not started yet");
             if (game.turn_order[0] !== user.name)
-                return on_err('not your turn yet');
+                return on_err("not your turn yet");
             if (!game.hand[user.name].includes(move))
-                return on_err('invalid move');
+                return on_err("invalid move");
         }.bind(null, lobby),
 };
 const actions = {
@@ -166,12 +168,12 @@ function get_all_cards() {
         all_card = all_card.concat([`b${i}`, `g${i}`, `r${i}`, `y${i}`]);
         all_card = all_card.concat([`b${i}`, `g${i}`, `r${i}`, `y${i}`]);
     }
-    for (let i of ['p', 'r', 's']) {
+    for (let i of ["p", "r", "s"]) {
         all_card = all_card.concat([`b${i}`, `g${i}`, `r${i}`, `y${i}`]);
         all_card = all_card.concat([`b${i}`, `g${i}`, `r${i}`, `y${i}`]);
     }
     for (let i = 0; i < 4; ++i) {
-        all_card = all_card.concat([`wc`, 'wf']);
+        all_card = all_card.concat([`wc`, "wf"]);
     }
     return all_card;
 }
