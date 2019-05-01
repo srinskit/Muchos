d0ddbdb6f10ce6ccd74f8c186e028dd00cbd3da8
srinskit <srinagrao2007@gmail.com>
2019-05-01 15:51:55 +0530

    Removed useless deps and boilerplate code

--- MuchosServer/app.js
+++ MuchosServer/app.js
@@ -1,22 +1,14 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const indexRouter = require("./routes/index");
 
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.post("/dfWebHook", function (req, res) {
     if (req.body.queryResult.intent.name === process.env.DIALOG_FLOW_INTENT_NAME) {
         console.log(req.body.queryResult.parameters.color[0]);
@@ -27,8 +19,7 @@ app.post("/dfWebHook", function (req, res) {
 
 // error handler
 app.use(function (req, res) {
-    res.status(404);
-    res.end();
+    res.sendStatus(404);
 });
 
 const dialogFlow = require('dialogflow');
